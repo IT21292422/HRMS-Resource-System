4a331d989449727db8151c2c7624e1db657dad27
Abdul Azeez Irfan <it21331022@my.sliit.lk>
2023-04-12 05:36:52 +0530

    fixedserver issues

--- server/server.js
+++ server/server.js
@@ -22,12 +22,12 @@ mongoose.connect(Db, {
     useUnifiedTopology: true
 });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const connection = mongoose.connection;
 connection.once("open", ()=> {
