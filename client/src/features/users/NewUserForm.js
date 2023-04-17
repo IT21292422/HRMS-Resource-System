c8d6b71039b18d2dc468db49bb4d9b9b1acf591e
Zaqwan <it21252518@my.sliit.lk>
2023-04-17 13:24:08 +0530

    I know github now

--- client/src/features/users/NewUserForm.js
+++ client/src/features/users/NewUserForm.js
@@ -198,7 +198,7 @@ const NewUserForm = () => {
         <>
 
             <button type="button" class="btn btn-primary col-2 " data-bs-toggle="modal" data-bs-target="#adduser">
-                Add Employee hello from abdul azeez helo
+                Add Employee
             </button>
             <div class="modal fade" id="adduser" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="ReqLeaveFormLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-xl">
