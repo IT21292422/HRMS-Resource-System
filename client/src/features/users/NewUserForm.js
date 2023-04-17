2a781a5bafd749ce892ae97dc2785d684d5b098b
Zaqwan <it21252518@my.sliit.lk>
2023-04-17 13:18:26 +0530

    zaqwan tetsing

--- client/src/features/users/NewUserForm.js
+++ client/src/features/users/NewUserForm.js
@@ -198,7 +198,7 @@ const NewUserForm = () => {
         <>
 
             <button type="button" class="btn btn-primary col-2 " data-bs-toggle="modal" data-bs-target="#adduser">
-                Add Employee Hello again Hello
+                Add Employee Hello again Hello helo
             </button>
             <div class="modal fade" id="adduser" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="ReqLeaveFormLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-xl">
